72f44cc2ea7d2c1b9c8bd9b4b82c8439458cb9ac
Facundo <git@facundo.io>
2017-09-21 14:04:54 -0600

    Vector: resizing, constructors and operator=

--- structures/vector.h
+++ structures/vector.h
@@ -2,32 +2,45 @@
 #define __STRUCTURES_VECTOR__
 
 #include <cstddef>
+#include <cassert>
 #include <algorithm>
 #include <memory>
+#include <initializer_list>
 #include <vector>
 
 template<typename T>
 class Vector {
 public:
-  Vector();
-  virtual ~Vector();
+  Vector(); // O(1)
+  Vector(const Vector& v); // O(n)
+  Vector(Vector&& rvr); // O(1)
+  Vector(std::initializer_list<T> l); // O(n)
+  Vector& operator=(const Vector& v); // O(n)
+  Vector& operator=(Vector&& rvr); // O(1)
+  virtual ~Vector(); // O(n)
 
-  std::size_t size() const;
-  std::size_t capacity() const;
-  bool empty() const;
-  T& at(std::size_t i) const;
-  T& back() const;
-  T& operator [](std::size_t i) const;
-  void push_back(const T& e);
-  void insert(std::size_t i, const T& e);
-  T pop_back();
-  T del(std::size_t i);
-  std::size_t find(const T& e) const;
-  std::size_t remove(const T& e);
-  std::vector<T> to_std_vector() const;
+  std::size_t capacity() const; // O(1)
+  std::size_t reserve(std::size_t new_capacity); // O(new_capacity)
 
-private:
-  std::size_t resize(std::size_t new_capacity);
+  std::size_t size() const; // O(1)
+  std::size_t resize(std::size_t new_size); // O(new_size)
+  bool empty() const; // O(1)
+
+  T& at(std::size_t i); // O(1)
+  const T& at(std::size_t i) const; // O(1)
+  T& operator[](std::size_t i); // O(1)
+  const T& operator[](std::size_t i) const; // O(1)
+  T& back(); // O(1)
+  const T& back() const; // O(1)
+  std::size_t find(const T& e) const; // O(n)
+
+  void push_back(const T& e); // worst case O(n), amortized O(1)
+  void insert(std::size_t i, const T& e); // worst case O(n), amortized O(1)
+  T pop_back(); // worst case O(n), amortized O(1)
+  T erase(std::size_t i); // worst case O(n), amortized O(1)
+  std::size_t remove(const T& e); // worst case O(n), amortized O(1)
+
+  std::vector<T> to_std_vector() const; // O(n)
 
 private:
   T* _arr;
@@ -36,7 +49,56 @@ private:
 };
 
 template<typename T>
-Vector<T>::Vector() : _arr(new T[0]), _size(0), _capacity(0) {
+Vector<T>::Vector() : _arr(new T[1]), _size(0), _capacity(1) {
+}
+
+#include <iostream>
+template<typename T>
+Vector<T>::Vector(std::initializer_list<T> l) : _arr(new T[l.size()]),
+_size(l.size()), _capacity(l.size()) {
+  // size will be > 0 (and therefore capacity) because of
+  // http://en.cppreference.com/w/cpp/language/list_initialization
+  // "If the braced-init-list is empty and T is a class type with a
+  // default constructor, value-initialization is performed."
+  std::move(l.begin(), l.end(), _arr);
+}
+
+template<typename T>
+Vector<T>::Vector(const Vector& v) : _arr(new T[v._capacity]), _size(v._size), _capacity(v._capacity) {
+  std::copy(v._arr, v._arr + v._size, _arr);
+}
+
+template<typename T>
+Vector<T>::Vector(Vector&& rvr) : _arr(rvr._arr), _size(rvr._size), _capacity(rvr._capacity) {
+  rvr._arr = new T[1];
+  rvr._size = 0;
+  rvr._capacity = 1;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(const Vector& v) {
+  // correctly handles self-assignment
+  T* newarr = new T[v._capacity];
+  std::copy(v._arr, v._arr + v._size, newarr);
+  delete[] _arr;
+  _arr = newarr;
+  _size = v._size;
+  _capacity = v._capacity;
+  return *this;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(Vector&& rvr) {
+  // correctly handles self-assignment
+  if(this != &rvr) {
+    _arr = rvr._arr;
+    _size = rvr._size;
+    _capacity = rvr._capacity;
+    rvr._arr = new T[1];
+    rvr._size = 0;
+    rvr._capacity = 1;
+  }
+  return *this;
 }
 
 template<typename T>
@@ -60,22 +122,33 @@ bool Vector<T>::empty() const {
 }
 
 template<typename T>
-T& Vector<T>::at(std::size_t i) const {
-  return _arr[i]; // or *(_arr + i)
+T& Vector<T>::at(std::size_t i) {
+  return _arr[i];
 }
 
 template<typename T>
-T& Vector<T>::back() const {
+const T& Vector<T>::at(std::size_t i) const {
+  return _arr[i];
+}
+
+template<typename T>
+const T& Vector<T>::back() const {
   return _arr[size() - 1];
 }
 
 template<typename T>
-T& Vector<T>::operator[](std::size_t i) const {
+const T& Vector<T>::operator[](std::size_t i) const {
   return at(i);
 }
 
 template<typename T>
-std::size_t Vector<T>::resize(std::size_t new_capacity) {
+T& Vector<T>::operator[](std::size_t i) {
+  return at(i);
+}
+
+// "resizes the capacity of the vector"
+template<typename T>
+std::size_t Vector<T>::reserve(std::size_t new_capacity) {
   // don't resize if new capacity cannot hold current elements
   if(new_capacity < size()) {
     return capacity();
@@ -86,14 +159,29 @@ std::size_t Vector<T>::resize(std::size_t new_capacity) {
   delete[] _arr;
   _arr = newarr;
   _capacity = new_capacity;
+  return capacity();
+}
 
+// resizes the array in such a way that size = capacity = new_size
+// if the new size is smaller than the current, the array is truncated,
+// if the new size is bigger than the current, new elements are allocated
+// using the default constructor of T
+template<typename T>
+std::size_t Vector<T>::resize(std::size_t new_size) {
+  T* newarr = new T[new_size];
+  std::move(_arr, _arr + std::min(new_size, _size), newarr);
+  delete[] _arr;
+  _arr = newarr;
+  _size = new_size;
+  _capacity = new_size;
   return capacity();
 }
 
 template<typename T>
 void Vector<T>::insert(std::size_t i, const T& e) {
+  assert(i <= size());
   if(size() >= capacity()) {
-    resize(std::max(capacity() * 2, (std::size_t)1));
+    reserve(capacity() * 2);
   }
 
   // shift elements if inserting in the middle,
@@ -117,7 +205,7 @@ void Vector<T>::push_back(const T& e) {
 }
 
 template<typename T>
-T Vector<T>::del(std::size_t i) {
+T Vector<T>::erase(std::size_t i) {
   T ret = _arr[i];
 
   // shift elements if removing from the middle,
@@ -136,7 +224,7 @@ T Vector<T>::del(std::size_t i) {
   // shrinking and growing constant (1/2) for the operations
   // to be O(1) amortized
   if(size() < capacity() / 4) {
-    resize(capacity() / 2);
+    reserve(capacity() / 2);
   }
 
   return ret;
@@ -144,7 +232,7 @@ T Vector<T>::del(std::size_t i) {
 
 template<typename T>
 T Vector<T>::pop_back() {
-  return del(size() - 1);
+  return erase(size() - 1);
 }
 
 template<typename T>
@@ -157,7 +245,7 @@ template<typename T>
 std::size_t Vector<T>::remove(const T& e) {
   std::size_t loc = find(e);
   if(loc != size()) {
-    del(loc);
+    erase(loc);
   }
   return loc;
 }
--- structures/vector.test.cc
+++ structures/vector.test.cc
@@ -17,8 +17,80 @@ int main(int argc, char const *argv[]) {
   th.tassert();
 
   {
+    th.message("Initializer list construction");
+    Vector<int> v = {1, 2, 3, 4};
+    th.tassert();
+    th.tassert(v.size(), (std::size_t)4, "Size is 4");
+    th.tassert(v.capacity(), (std::size_t)4, "Capacity is 4");
+    th.message("Destruction");
+  }
+  th.tassert();
+
+  {
+    Vector<int> v = {1, 2, 3, 4};
+    th.message("Copy construction");
+    Vector<int> v2(v);
+    th.tassert();
+    th.tassert(v.to_std_vector() == v2.to_std_vector(), true, "STD vectors equal");
+    th.message("Destruction");
+  }
+  th.tassert();
+
+  {
+    th.message("Move semantics construction");
+    Vector<int> v2(Vector<int>{1, 2, 3, 4});
+    th.tassert();
+    th.tassert(v2.to_std_vector() == std::vector<int>{1, 2, 3, 4}, true, "STD vectors equal");
+    th.message("Destruction");
+  }
+  th.tassert();
+
+  {
+    Vector<int> v = {1, 2, 3, 4};
+    th.message("operator= (copy)");
+    Vector<int> v2;
+    v2 = v;
+    th.tassert();
+    th.tassert(v.to_std_vector() == v2.to_std_vector(), true, "STD vectors equal");
+    th.message("Destruction");
+  }
+  th.tassert();
+
+  {
+    th.message("operator= (move)");
+    Vector<int> v2;
+    v2 = Vector<int>{1, 2, 3, 4};
+    th.tassert();
+    th.tassert(v2.to_std_vector() == std::vector<int>{1, 2, 3, 4}, true, "STD vectors equal");
+    th.message("Destruction");
+  }
+  th.tassert();
+
+  {
+    Vector<int> v = {1, 2, 3, 4};
+    th.message("Resize (shrink) to 2");
+    v.resize(2);
+    th.tassert();
+    th.tassert(v.size(), (std::size_t)2, "Size is 2");
+    th.tassert(v.capacity(), (std::size_t)2, "Capacity is 2");
+    th.tassert(v.at(0), 1, "v.at(0) == 1");
+    th.tassert(v.at(1), 2, "v.at(1) == 2");
+    th.message("Resize (grow) to 8");
+    v.resize(8);
+    th.tassert();
+    th.tassert(v.size(), (std::size_t)8, "Size is 8");
+    th.tassert(v.capacity(), (std::size_t)8, "Capacity is 8");
+    th.tassert(v.at(0), 1, "v.at(0) == 1");
+    th.tassert(v.at(1), 2, "v.at(1) == 2");
+    th.message("set v[4] = 7");
+    v[4] = 7;
+    th.tassert(v.at(4), 7, "v.at(4) == 7");
+    th.tassert();
+  }
+
+  {
     Vector<int> v;
-    th.tassert(v.capacity(), (std::size_t)0, "Initial capacity is 0");
+    th.tassert(v.capacity(), (std::size_t)1, "Initial capacity is 1");
 
     v.push_back(7);
     th.tassert(v.capacity(), (std::size_t)1, "Capacity after one push is 1");
@@ -83,7 +155,7 @@ int main(int argc, char const *argv[]) {
     th.tassert(v.capacity(), (std::size_t)8, "Capacity is 8");
 
     th.message("Delete index 0");
-    int elem_at_zero = v.del(0);
+    int elem_at_zero = v.erase(0);
     th.tassert();
     th.tassert(elem_at_zero, 1, "Element at index 0 was 1");
     th.tassert(v[0], 7, "v[0] == 7");
@@ -96,10 +168,22 @@ int main(int argc, char const *argv[]) {
     th.tassert(v[0], 27);
 
     th.message("Delete index 0");
-    v.del(0);
+    v.erase(0);
     th.tassert();
     th.tassert(v.size(), (std::size_t)1, "Size is 1");
     th.tassert(v.capacity(), (std::size_t)4, "Capacity is 4");
+
+    th.message("Pop last element");
+    v.pop_back();
+    th.tassert();
+    th.tassert(v.size(), (std::size_t)0, "Size is 0");
+    th.tassert(v.capacity(), (std::size_t)2, "Capacity is 2");
+
+    th.message("Push after having emptied");
+    v.push_back(55);
+    th.tassert();
+    th.tassert(v.capacity(), (std::size_t)2, "Capacity is 2");
+    th.tassert(v.size(), (std::size_t)1, "Size is 1");
   }
 
   std::srand((unsigned int)std::time(0));
@@ -115,6 +199,7 @@ int main(int argc, char const *argv[]) {
       int r = std::rand();
       v.push_back(r);
       stdv.push_back(r);
+      th.tassert(v.to_std_vector() == stdv, true, "Check complete vector", true);
     }
     th.tassert();
 
@@ -127,8 +212,9 @@ int main(int argc, char const *argv[]) {
       int b = stdv.back();
       stdv.pop_back();
       if(a != b) {
-        th.tassert(a, b);
+        th.tassert(a, b, "Element returned by pop");
       }
+      th.tassert(v.to_std_vector() == stdv, true, "Check complete vector", true);
     }
     th.tassert();
 
@@ -138,7 +224,7 @@ int main(int argc, char const *argv[]) {
         th.tassert(false);
       }
       unsigned int idx = std::rand() % v.size();
-      v.del(idx);
+      v.erase(idx);
       stdv.erase(stdv.begin() + idx);
     }
     th.tassert();
