59594692b65b4c36b85adb7ea8075e9fb387788d
Facundo <git@facundo.io>
2017-11-05 19:24:34 -0700

    Rabin-Karp string matching

--- algos/string_search.cc
+++ algos/string_search.cc
@@ -1,8 +1,9 @@
-#include <cmath>
 #include <cstddef>
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <queue>
 #include "../test_helpers.h"
 
 // #define LARGE_PRIME 1000000007
@@ -52,6 +53,10 @@ public:
     }
   }
 
+  int length() const {
+    return _length;
+  }
+
   uint64_t hash() const {
     return _hash;
   }
@@ -61,19 +66,59 @@ private:
   int _length;
 };
 
+template<class C>
+std::vector<int> rabin_karp(const std::string& pattern, const C& stream) {
+  RollingHash phash;
+  phash.append(pattern);
+
+  RollingHash chash;
+  std::deque<char> q;
+  std::vector<int> places;
+  int i = 0;
+
+  for (const auto& e : stream) {
+    chash.append(e);
+    q.push_back(e);
+
+    if (q.size() > pattern.size()) {
+      const auto& front = q.front();
+      chash.pop(front);
+      q.pop_front();
+    }
+
+    i++;
+
+    // las vegas method
+    if (chash.hash() == phash.hash() && std::string(q.begin(), q.end()) == pattern) {
+      places.push_back(i - pattern.size());
+    }
+  }
+
+  return places;
+}
+
 int main(int argc, char const *argv[]) {
   TestHelper th;
-  std::srand((unsigned int)std::time(0));
 
-  RollingHash rh;
-  RollingHash f;
+  {
+    RollingHash rh;
+    RollingHash f;
 
-  f.append("alongtimeago");
-  rh.append("123456789012345678901234567890####****alongtimeago");
-  rh.pop("123456789012345678901234567890####****");
+    f.append("alongtimeago");
+    rh.append("123456789012345678901234567890####****alongtimeago");
+    rh.pop("123456789012345678901234567890####****");
 
-  std::cout << "f.hash()  : 0x" << std::hex << f.hash() << std::endl;
-  std::cout << "rh.hash() : 0x" << std::hex << rh.hash() << std::endl;
+    std::cout << "f.hash()  : 0x" << std::hex << f.hash() << std::endl;
+    std::cout << "rh.hash() : 0x" << std::hex << rh.hash() << std::endl;
+  }
+
+  {
+    auto places = rabin_karp("ABABA", "456ABABABABABA123BA");
+    std::cout << "Rabin-Karp(ABABA, 456ABABABABABA123BA): ";
+    for (const auto& e : places) {
+      std::cout << e << ", ";
+    }
+  }
 
   th.summary();
   return 0;
